01ae029a97b0dc046ab40c7b7746d1634d197e23
BhanuThapa <bhanuthapa252@gmail.com>
2024-01-30 02:48:38 +0530

    Responsive design media query Done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const movieDetails = document.querySelector('.movie-details');
 const image = document.querySelector('.image');
+const err = document.querySelector('.error');
 
 let input = document.querySelector('input');
 
@@ -18,26 +19,42 @@ const api = 'https://www.omdbapi.com/?apikey=a7fb5e62&t=';
 
 function search() {
   let query = api + input.value;
-  movieDetails.style.visibility = 'visible';
-  image.style.visibility = 'visible';
 
-  try {
-    fetch(query)
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        Title.innerText = data.Title;
-        Year.innerText = data.Year;
-        Released.innerText = data.Released;
-        Genre.innerText = data.Genre;
-        Director.innerText = data.Director;
-        Writer.innerText = data.Writer;
-        Actors.innerText = data.Actors;
-        Plot.innerText = data.Plot;
-        Language.innerText = data.Language;
-        Ratings.innerText = data.Ratings[0].Value;
-        Poster.src = data.Poster;
-      });
-  } catch (error) {}
+  fetch(query)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network error');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      err.style.display = 'none';
+      movieDetails.style.display = 'block';
+      image.style.display = 'block';
+
+      Title.innerText = data.Title;
+      Year.innerText = data.Year;
+      Released.innerText = data.Released;
+      Genre.innerText = data.Genre;
+      Director.innerText = data.Director;
+      Writer.innerText = data.Writer;
+      Actors.innerText = data.Actors;
+      Plot.innerText = data.Plot;
+      Language.innerText = data.Language;
+      Ratings.innerText = data.Ratings[0].Value;
+      Poster.src = data.Poster;
+    })
+    .catch((error) => {
+      // if (error instanceof TypeError && error.message.includes('API key')) {
+      //   console.error('Invalid API key:', error);
+      // } else {
+
+      movieDetails.style.display = 'none';
+      image.style.display = 'none';
+      err.style.display = 'flex';
+
+      console.log('Movie not found', error);
+
+      // }
+    });
 }
